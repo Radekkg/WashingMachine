42158a3ea9e6011a1d6db65ffe363625e232956f
CapTechnomate <radoslaw.lewek@capgemini.com>
2023-11-03 16:12:02 +0100

    building an interface with methods from the documentation

---
+++ src/main/java/org/radoslaw/Washing.java
@@ -0,0 +1,22 @@
+package org.radoslaw;
+
+public interface Washing {
+    void setProgram(int programNumber);
+    int getProgram();
+    void nextProgram();
+    void previousProgram();
+    void increaseTemp();
+    void decreaseTemp();
+    void setTemperature(double temperature);
+    double getTemperature();
+    void setSpine(int spine);
+    int getSpine();
+    void increaseSpine();
+    void decreaseSpine();
+    void showStatus();
+    void setDelay(int hours);
+    int getDelay();
+    void increaseDelay();
+    void decreaseDelay();
+
+}
