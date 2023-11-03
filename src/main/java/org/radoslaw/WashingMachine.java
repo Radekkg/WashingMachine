f8a3f7dbad9b8f679839c2a38fa1c78ad4bf3347
CapTechnomate <radoslaw.lewek@capgemini.com>
2023-11-03 16:22:58 +0100

    creation of the WashingMachine class and implementation of the Washable interface

---
+++ src/main/java/org/radoslaw/WashingMachine.java
@@ -0,0 +1,88 @@
+package org.radoslaw;
+
+public class WashingMachine implements Washable {
+    @Override
+    public void setProgram(int programNumber) {
+
+    }
+
+    @Override
+    public int getProgram() {
+        return 0;
+    }
+
+    @Override
+    public void nextProgram() {
+
+    }
+
+    @Override
+    public void previousProgram() {
+
+    }
+
+    @Override
+    public void increaseTemp() {
+
+    }
+
+    @Override
+    public void decreaseTemp() {
+
+    }
+
+    @Override
+    public void setTemperature(double temperature) {
+
+    }
+
+    @Override
+    public double getTemperature() {
+        return 0;
+    }
+
+    @Override
+    public void setSpine(int spine) {
+
+    }
+
+    @Override
+    public int getSpine() {
+        return 0;
+    }
+
+    @Override
+    public void increaseSpine() {
+
+    }
+
+    @Override
+    public void decreaseSpine() {
+
+    }
+
+    @Override
+    public void showStatus() {
+
+    }
+
+    @Override
+    public void setDelay(int hours) {
+
+    }
+
+    @Override
+    public int getDelay() {
+        return 0;
+    }
+
+    @Override
+    public void increaseDelay() {
+
+    }
+
+    @Override
+    public void decreaseDelay() {
+
+    }
+}
